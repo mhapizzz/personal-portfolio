3c00c6b9435ccbc6b4540eb6edf5fe891323f7f5
mhapizzz <mhapiz10@gmail.com>
2026-09-21 14:00:30 +0700

    Fix Vue Router base for GitHub Pages.

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import AboutView from '../views/AboutView.vue'
 import ContactView from '../views/ContactView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/projects', name: 'projects', component: ProjectsView },
